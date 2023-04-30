21394d17b6c584eeb33a7cf48b20ccda8c202ff0
oscaroguzhan <72919227+oscaroguzhan@users.noreply.github.com>
2023-04-30 11:39:29 +0200

    type definitions

--- README.md
+++ README.md
@@ -22,3 +22,27 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+##
+Reactive property with composition API
+```
+const state = reactive({
+  title: 'Find your dream work with CodeGuruOzzy',
+
+  })
+//toRef use to create a ref for a property on source reactive object
+  return { ...toRefs(state) }
+```
+
+## 
+Ref property with 
+```
+! KEEP IN MIND YOU NEED TO USE GENERIC ARGUMENTS TO SPECIFY THE TYPE OF THE VARIABLE AND YOU NEED TO USE .value property to change the value of the variable 
+
+  const title = ref('Oscar');
+  const age = ref<number>(25)
+
+  return {title ,age}
+
+
+```
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template>
   <div id="app">
-    hello
+    <h2 class="title">
+      {{ jobs[0].id }}
+    </h2>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-
+import { defineComponent, ref } from
+  'vue';
+import Job from './types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {
+  components: {},
+  setup() {
+    
+    // TYPE DEFINITION AND IMPORT JOB INTERFACE 
+    const jobs = ref<Job[]>([
+      { title: 'front-end developer', location: 'Sweden', salary: 35000, id: '1' },
+      { title: 'full-stack developer', location: 'Denmark', salary: 38000, id: '2' },
+      { title: 'JR front-end developer', location: 'Sweden', salary: 32000, id: '3' },
+      { title: 'Sales Manager', location: 'Sweden', salary: 38000, id: '4' }
+    ])
+    return {jobs}
   }
-});
+},
+);
 </script>
 
-<style lang="scss">
- #app {
+<style>
+#app {
   display: flex;
   align-items: center;
   justify-content: center;
- }
+}
+
+.title {
+  font-weight: 500;
+  font-style: italic;
+}
 </style>
